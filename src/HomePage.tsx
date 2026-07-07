bbf252f7d8b2f3ebd8d1673bb1b35a3a282f7832
katerinaiov <katerina.iov@nikitec.gr>
2026-07-07 15:35:06 +0300

    Add partners details

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,10 +1,25 @@
+
+import React, { useState } from "react";
+
 import {
   Row,
   Col,
   Typography,
   Card,
   List,
+  Modal,
 } from "antd";
+
+
+import twtLogo from './assets/twt.png';
+import fraunhoferLogo from './assets/fraunhofer.png';
+import kassbohrerLogo from './assets/Kassbohrer.jpg';
+import MarquadtLogo from './assets/Marquardt.svg';
+import siemensLogo from './assets/siemens.svg';
+import universityLogo from './assets/university.png';
+import youseLogo from './assets/youse.svg';
+import RekonikLogo from './assets/Picture1.png';
+
 
 import Section from "./components/Section";
 import RightNavigation from "./components/RightNavigation";
@@ -14,6 +29,55 @@ import RightNavigation from "./components/RightNavigation";
 const { Title, Paragraph } = Typography;
 
 const HomePage = () => {
+
+const [selectedPartner, setSelectedPartner] = useState<any>(null);
+
+const partnerInfo = {
+  twt: {
+    name: "TWT GmbH",
+    role: "Coordinator",
+    contact: "Dr. Martin Obstbaum",
+    email: "martin.obstbaum@twt-gmbh.de",
+    address: "Industriestraße 6, 70565 Stuttgart",
+  },
+
+  fraunhofer: {
+    name: "Fraunhofer IAO",
+    role: "Research Partner",
+    contact: "Benjamin Schneider",
+    address: "Nobelstraße 12, 70569 Stuttgart",
+  },
+
+  tub: {
+    name: "TU Berlin",
+    role: "Research Partner",
+    contact: "Dr. Carina Fresemann",
+  },
+
+  youse: {
+    name: "YOUSE GmbH",
+    role: "Research Partner",
+    contact: "Dr. Sebastian Glende Meinzer",
+  },
+
+  marquardt: {
+    name: "Marquardt GmbH",
+    role: "Industrial Partner",
+    contact: "Alexander Schneider",
+  },
+
+  siemens: {
+    name: "Siemens AG",
+    role: "Industrial Partner",
+    contact: "Dr. Rohit Gupta",
+  },
+
+  kassbohrer: {
+    name: "Kässbohrer",
+    role: "Industrial Partner",
+    contact: "Sven Jurak",
+  },
+};
   return (
     <>
       <RightNavigation />
@@ -35,7 +99,7 @@ const HomePage = () => {
           </Col>
 
           <Col span={12}>
-            <img src = "./Picture1.png"/>
+            <img src = {RekonikLogo}/>
           </Col>
         </Row>
       </Section>
@@ -134,46 +198,82 @@ const HomePage = () => {
         <Row gutter={[24, 24]}>
           <Col span={2}>
               <div style={{ display:'flex', alignItems:'center', gap:'12px'}}>
-              <img src="./twt.png" style={{ width:'60px',height:'30px'}}/>
+              <img src= {twtLogo} style={{ width:'60px',height:'30px', cursor: "pointer"}} onClick={() => setSelectedPartner(partnerInfo.twt)}/>
              </div>
           </Col>
 
           <Col span={2.5}>
               <div style={{ display:'flex', alignItems:'center', gap:'12px'}}>
-              <img src="./siemens.svg" style={{ width:'auto',height:'30px'}}/>
+            <img src={siemensLogo} style={{ width:'auto',height:'30px', cursor: "pointer"}} onClick={() => setSelectedPartner(partnerInfo.siemens)}/>
               </div>
           </Col>
 
           <Col span={2.5}>
               <div style={{ display:'flex', alignItems:'center', gap:'12px'}}>
-              <img src="./fraunhofer.png" style={{ width:'auto',height:'30px'}}/>
+              <img src={fraunhoferLogo} style={{ width:'auto',height:'30px', cursor: "pointer"}} onClick={() => setSelectedPartner(partnerInfo.fraunhofer)}/>
               </div>
           </Col>
 
           <Col span={2}>
               <div style={{ display:'flex', alignItems:'center', gap:'12px'}}>
-              <img src="./Marquardt.svg" style={{ width:'150px',height:'30px'}}/>
+              <img src={MarquadtLogo} style={{ width:'150px',height:'30px', cursor: "pointer"}} onClick={() => setSelectedPartner(partnerInfo.marquardt)}/>
               </div>
           </Col>
 
           <Col span={2.5}>
               <div style={{ display:'flex', alignItems:'center', gap:'12px'}}>
-              <img src="./Kassbohrer.jpg" style={{ width:'auto',height:'30px'}}/>
+              <img src={kassbohrerLogo} style={{ width:'auto',height:'30px', cursor: "pointer"}} onClick={() => setSelectedPartner(partnerInfo.kassbohrer)}/>
               </div>
           </Col>
 
           <Col span={2.5}>
               <div style={{ display:'flex', alignItems:'center', gap:'12px'}}>
-            <img src="./university.png" style={{ width:'auto',height:'30px'}}/>
+            <img src={universityLogo} style={{ width:'auto',height:'30px', cursor: "pointer"}} onClick={() => setSelectedPartner(partnerInfo.tub)}/>
             </div>
           </Col>
 
           <Col span={2.5}>
               <div style={{ display:'flex', alignItems:'center', gap:'12px'}}>
-              <img src="./youse.svg" style={{ width:'auto',height:'30px'}}/>
+              <img src={youseLogo} style={{ width:'auto',height:'30px', cursor: "pointer"}} onClick={() => setSelectedPartner(partnerInfo.youse)}/>
               </div>
           </Col>
         </Row>
+       {selectedPartner && (
+  <Row
+    style={{
+      marginTop: "50px",
+      paddingTop: "30px",
+      borderTop: "1px solid #bdbdbd",
+    }}
+  >
+    <Col span={8}>
+      {selectedPartner.logo}
+    </Col>
+
+    <Col span={16}>
+      <Title level={3}>
+        {selectedPartner.name}
+      </Title>
+
+      <Paragraph>
+        <strong>Role:</strong> {selectedPartner.role}
+      </Paragraph>
+
+      <Paragraph>
+        <strong>Contact:</strong> {selectedPartner.contact}
+      </Paragraph>
+
+      <Paragraph>
+        <strong>Address:</strong> {selectedPartner.address}
+      </Paragraph>
+
+      <Paragraph>
+        <strong>Email:</strong> {selectedPartner.email}
+      </Paragraph>
+    </Col>
+  </Row>
+)}
+
       </Section>
 
       <Section
